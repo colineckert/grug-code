4c66e3c55a424d1eceba0f96e25533420dc15a3f
Colin Eckert <colineckert10@gmail.com>
2025-06-11 06:33:39 -0700

    Refactor and define message content and role

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
 
-client = genai.Client(api_key=api_key)
+def main():
+    load_dotenv()
 
-response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
-)
+    args = sys.argv[1:]
 
-print(response.text)
-print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-print("Response tokens:", response.usage_metadata.candidates_token_count)
+    if not args:
+        print("Grug Code Assistant")
+        print("Usage: python main.py <input_text>")
+        exit(1)
+
+    user_prompt = " ".join(args)
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    messages = [
+        types.Content(role='user', parts=[types.Part(text=user_prompt)]),
+    ]
+
+    generate_content(client, messages)
+
+
+def generate_content(client, messages):
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', contents=messages
+    )
+    print(response.text)
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
+
+
+if __name__ == "__main__":
+    main()
